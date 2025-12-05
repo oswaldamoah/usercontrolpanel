d8a937cf4db306654cc90ee50baf9e0d78da14b3
oswaldamoah <oswaldamoah00@gmail.com>
2025-12-05 04:22:40 +0000

    added components and set up routes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard'
+import Header from './components/Header'
+import Login from './components/Login'
+import Preferences from './components/Preferences'
+import SessionInfo from './components/SessionInfo'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Define individual routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/preferences" element={<Preferences />} />
+        <Route path="/session" element={<SessionInfo />} />
+
+        {/* Catch-all route for 404 pages (should be the last route) */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
   return (
     <>
       
